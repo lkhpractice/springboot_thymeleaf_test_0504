3571d0db2a9417e176145ce1eba5c8b7052e457d
ici <ici@DESKTOP-CL6HLKL>
2023-05-04 16:11:15 +0900

    백업

--- src/main/java/com/lkhpractice/thymeleaf/TestController.java
+++ src/main/java/com/lkhpractice/thymeleaf/TestController.java
@@ -1,5 +1,7 @@
 package com.lkhpractice.thymeleaf;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +19,34 @@ public class TestController {
 		model.addAttribute("memberDto", dto);
 		
 		return "hello";
+	}
+	
+	@RequestMapping(value = "/iftest")
+	public String iftest(Model model) {
+		
+		MemberDto dto = new MemberDto("김유신", 27);
+		
+		model.addAttribute("memberDto", dto);
+		
+		return "iftest";
+	}
+	
+	@RequestMapping(value = "/eachtest")
+	public String eachtest(Model model) {
+		
+		MemberDto dto1 = new MemberDto("김유신", 22);
+		MemberDto dto2 = new MemberDto("이순신", 23);
+		MemberDto dto3 = new MemberDto("강감찬", 17);
+		
+		ArrayList<MemberDto> dtos = new ArrayList<MemberDto>();
+		
+		dtos.add(dto1);
+		dtos.add(dto2);
+		dtos.add(dto3);
+		
+		model.addAttribute("memberDtos", dtos);
+		
+		return "eachTest";
+		
 	}
 }
