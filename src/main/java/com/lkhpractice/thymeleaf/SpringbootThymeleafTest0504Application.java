72fc081490b89e374bf5f1e1340a31f0fbdeb3d0
ici <ici@DESKTOP-CL6HLKL>
2023-05-04 15:02:44 +0900

    백업

---
+++ src/main/java/com/lkhpractice/thymeleaf/SpringbootThymeleafTest0504Application.java
@@ -0,0 +1,13 @@
+package com.lkhpractice.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafTest0504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafTest0504Application.class, args);
+	}
+
+}
